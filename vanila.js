a749ece1b37c74a93849fb3f6117c32e648cbc03
Alsandre <gabeditebuli@gmail.com>
2021-09-18 18:33:08 +0400

    feat: implement layout

--- vanila.js
+++ vanila.js
@@ -1,18 +1,27 @@
 $(document).ready(() => {
+    
     $(".start-button").on('click', () => {
         $(".landing-page").css("display", "none");
         $(".survey-wraper").css("display", "unset");
         $(".cover").css("animation", "popUp 0.5s linear")
     })
-    $(".first-page form .arrow-forward").click(()=>{
+    $("#first-arrow-forward").click((e)=>{
         // if($(".first-page form input[name='name']"))
+        e.preventDefault();
         if(document.getElementById("name").value && 
            document.getElementById("lastName").value &&
            document.getElementById("email").value){
                $(".first-page").css("display", "none");
-               $(".second-page").css("display", "unset");
+               $(".second-page").css("display", "flex");
            }
     })
+    $("#second-arrow-forward").click((e)=>{
+        e.preventDefault();
+
+        $(".second-page").css("display", "none");
+        $(".third-page").css("display", "flex");
+    })
+
     $(".second-page input[name='covid']").click(()=>{
         let covidValue = document.getElementsByName("covid");
         switch (true) {
@@ -21,9 +30,11 @@ $(document).ready(() => {
                 break; 
             case covidValue[1].checked:
                 $(".second-page .optional-covidPositive").css("display", "none");
+                $(".second-page .optional-pcrPositive").css("display", "none");
                 break; 
             case covidValue[2].checked:
                 $(".second-page .optional-covidPositive").css("display", "none");
+                $(".second-page .optional-pcrPositive").css("display", "none");
                 break; 
             default:
                 break;    
@@ -35,4 +46,27 @@ $(document).ready(() => {
             $(".optional-pcrPositive").css("display", "unset");
         }
     })
+
+    $("input[name='pcr-test']").click(()=>{
+        let pcrValue = document.getElementsByName("pcr-test");
+        switch (true) {
+            case pcrValue[0].checked:
+                $(".third-page .optional-pcrLevel").css("display", "unset");
+                break; 
+            case pcrValue[1].checked:
+                $(".third-page .optional-pcrLevel").css("display", "none");
+                break;
+            default:
+                break;    
+        }
+    })
+    $("input[name='pcr-test-stage']").click(()=>{
+        let pcrLevelValue = document.getElementsByName("pcr-test-stage");
+        if(pcrLevelValue[2].checked){
+            $("#optional-pcrFirstOnly").css("display", "unset");
+        }else{
+            $("#optional-pcrFirstOnly").css("display", "none");
+        }
+    })
+
 })
